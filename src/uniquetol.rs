ba5e4d13e15b45e96d5633ed324e853d86466494
Luis-Varona <lbvarona@mta.ca>
2025-03-24 22:50:02 -0300

    Refined uniquetol and unit tests; fleshed out enums, structs
    For small structs/enums, changed from pass-by-ref to pass-by-value. Also added derived traits (e.g., Debug, Clone, etc.). Finally, added the option for equal_nan in both isapprox and uniquetol, further developing unit tests (still in progress) accordingly.

--- src/uniquetol.rs
+++ src/uniquetol.rs
@@ -1,20 +1,38 @@
 #[path = "isapprox.rs"] mod isapprox;
 #[path = "test_arr.rs"] mod test_arr;
 
-use isapprox::{Tols, isapprox};
+use crate::isapprox::{EqualNan, Tols, isapprox};
 
-#[derive(Default, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
 pub enum Occurrence {
     #[default]
     Lowest,
     Highest,
 }
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct UniqueTolArray {
     pub arr_unique: Vec<f64>,
     pub indices_unique: Vec<usize>,
     pub inverse_unique: Vec<usize>,
     pub counts_unique: Vec<usize>,
+}
+
+impl UniqueTolArray {
+    #[inline]
+    pub fn remap_to_original(&self) -> Vec<f64> {
+        self.inverse_unique.iter().map(|&idx| self.arr_unique[idx]).collect()
+    }
+    
+    #[inline]
+    pub fn get_len_unique(&self) -> usize {
+        self.arr_unique.len()
+    }
+    
+    #[inline]
+    pub fn get_len_original(&self) -> usize {
+        self.inverse_unique.len()
+    }
 }
 
 fn sortperm(arr: &[f64], reverse: bool) -> Vec<usize> {
@@ -29,7 +47,7 @@ fn sortperm(arr: &[f64], reverse: bool) -> Vec<usize> {
     perm_sorted
 }
 
-pub fn uniquetol(arr: &[f64], tols: &Tols, occurrence: &Occurrence) -> UniqueTolArray {
+pub fn uniquetol(arr: &[f64], tols: Tols, equal_nan: EqualNan, occurrence: Occurrence) -> UniqueTolArray {
     let n = arr.len();
     
     if n == 0 {
@@ -41,7 +59,7 @@ pub fn uniquetol(arr: &[f64], tols: &Tols, occurrence: &Occurrence) -> UniqueTol
         };
     }
     
-    let perm_sorted = match *occurrence {
+    let perm_sorted = match occurrence {
         Occurrence::Lowest => sortperm(arr, false),
         Occurrence::Highest => sortperm(arr, true),
     };
@@ -57,7 +75,7 @@ pub fn uniquetol(arr: &[f64], tols: &Tols, occurrence: &Occurrence) -> UniqueTol
     for (i, &idx) in perm_sorted.iter().enumerate().skip(1) {
         let val = arr[idx];
         
-        match isapprox(val_curr, val, tols) {
+        match isapprox(val_curr, val, tols, equal_nan) {
             true => cnt_curr += 1,
             false => {
                 indices_unique.push(perm_sorted[idx_curr]);
@@ -74,7 +92,7 @@ pub fn uniquetol(arr: &[f64], tols: &Tols, occurrence: &Occurrence) -> UniqueTol
         }
     }
     
-    indices_unique.push(idx_curr);
+    indices_unique.push(perm_sorted[idx_curr]);
     counts_unique.push(cnt_curr);
     indices_unique.shrink_to_fit();
     counts_unique.shrink_to_fit();
@@ -103,57 +121,53 @@ mod tests {
     use super::*;
     use test_arr::TEST_ARR;
     
-    #[test]
-    fn test_uniquetol_lowest() {
+    fn test_uniquetol(occurrence: Occurrence) {
+        let n = TEST_ARR.len();
+        let k: usize = 179;
+        
+        let tols = Tols::default();
+        let equal_nan = EqualNan::default();
+        
         let uniquetol_arr = uniquetol(
             &TEST_ARR,
-            &Tols::default(),
-            &Occurrence::default(),
+            tols,
+            equal_nan,
+            occurrence,
         );
         
-        let n = TEST_ARR.len();
-        let k = uniquetol_arr.arr_unique.len();
-        assert_eq!(n, 729);
-        assert_eq!(k, 179);
+        assert_eq!(uniquetol_arr.get_len_unique(), k);
+        assert_eq!(uniquetol_arr.get_len_original(), n);
+        assert_eq!(uniquetol_arr.counts_unique.iter().sum::<usize>(), n);
         
-        println!("Length of the original input array: {}\n", n);
-        println!("Number of unique elements within tolerance: {}\n", k);
-        println!("Unique elements: {:?}\n", uniquetol_arr.arr_unique);
-        println!(
-            "Indices of the unique elements in the original array: {:?}\n",
-            uniquetol_arr.indices_unique
-        );
-        println!(
-            "Indices of the original elements in the unique array: {:?}\n",
-            uniquetol_arr.inverse_unique
-        );
-        println!("Counts of unique elements: {:?}\n", uniquetol_arr.counts_unique);
+        let arr_unique = &uniquetol_arr.arr_unique;
+        match occurrence {
+            Occurrence::Lowest => assert!(arr_unique.is_sorted()),
+            Occurrence::Highest => assert!(arr_unique.iter().rev().is_sorted()),
+        }
+        
+        let is_unique = arr_unique
+            .windows(2)
+            .all(|w| !isapprox(w[0], w[1], tols, equal_nan));
+        assert!(is_unique);
+        
+        let mapped_correctly = uniquetol_arr.indices_unique.iter().zip(arr_unique.iter())
+            .all(|(&idx, &x)| isapprox(TEST_ARR[idx], x, tols, equal_nan));
+        assert!(mapped_correctly);
+        
+        let arr_remapped = uniquetol_arr.remap_to_original();
+        let remapped_correctly = arr_remapped.len() == n
+            && TEST_ARR.iter().zip(arr_remapped.iter())
+                .all(|(&x, &y)| isapprox(x, y, tols, equal_nan));
+        assert!(remapped_correctly);
+    }
+    
+    #[test]
+    fn test_uniquetol_lowest() {
+        test_uniquetol(Occurrence::Lowest);
     }
     
     #[test]
     fn test_uniquetol_highest() {
-        let uniquetol_arr = uniquetol(
-            &TEST_ARR,
-            &Tols::default(),
-            &Occurrence::Highest,
-        );
-        
-        let n = TEST_ARR.len();
-        let k = uniquetol_arr.arr_unique.len();
-        assert_eq!(n, 729);
-        assert_eq!(k, 179);
-        
-        println!("Length of the original input array: {}\n", n);
-        println!("Number of unique elements within tolerance: {}\n", k);
-        println!("Unique elements: {:?}\n", uniquetol_arr.arr_unique);
-        println!(
-            "Indices of the unique elements in the original array: {:?}\n",
-            uniquetol_arr.indices_unique
-        );
-        println!(
-            "Indices of the original elements in the unique array: {:?}\n",
-            uniquetol_arr.inverse_unique
-        );
-        println!("Counts of unique elements: {:?}\n", uniquetol_arr.counts_unique);
+        test_uniquetol(Occurrence::Highest);
     }
 }
