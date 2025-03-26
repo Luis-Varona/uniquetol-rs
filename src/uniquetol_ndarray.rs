b0183a11a56e5a1cb50f6cb6277a2f3f27b61951
Luis-Varona <lbvarona@mta.ca>
2025-03-26 02:13:34 -0300

    Continued prototyping ndarray implementation
    Changed Axis enum to AxisFlatten, avoiding namespace conflicts with ndarray. Also refactored some logic, especially in the early return case of the uniquetol_ndarray function.

--- src/uniquetol_ndarray.rs
+++ src/uniquetol_ndarray.rs
@@ -1,24 +1,28 @@
 #[path = "isapprox.rs"] mod isapprox;
 #[path = "uniquetol.rs"] mod uniquetol;
 
-use ndarray::{Array, IxDyn};
+use ndarray::{Array, Axis, IxDyn};
 use crate::isapprox::{EqualNan, Tols};
 use crate::uniquetol::{Occurrence, UniqueTolArray, uniquetol};
 
-#[derive(Debug, Clone, Copy, PartialEq, Default)]
-pub enum Axis {
-    #[default]
-    All,
-    Dim(usize),
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum AxisFlatten {
+    Dim(Option<usize>),
 }
 
-impl Axis {
+impl AxisFlatten {
     pub fn new(dim: Option<usize>, d: usize) -> Result<Self, String> {
         match dim {
-            None => Ok(Axis::All),
-            Some(value) if value < d => Ok(Axis::Dim(value)),
-            Some(_) => Err(format!("Dim value must be between 0 and {}", d - 1)),
+            None => Ok(AxisFlatten::Dim(None)),
+            Some(axis) if axis < d => Ok(AxisFlatten::Dim(Some(axis))),
+            Some(_) => Err(format!("Dim axis must be between 0 and {} (inclusive)", d - 1)),
         }
+    }
+}
+
+impl Default for AxisFlatten {
+    fn default() -> Self {
+        AxisFlatten::Dim(None)
     }
 }
 
@@ -27,20 +31,18 @@ pub fn uniquetol_ndarray(
     tols: Tols,
     equal_nan: EqualNan,
     occurrence: Occurrence,
-    axis: Axis,
+    axis_flatten: AxisFlatten,
 ) -> Array<f64, IxDyn> {
-    if axis == Axis::All {
-        let arr_flat = arr.flatten().to_vec();
-        let arr_unique = uniquetol(&arr_flat, tols, equal_nan, occurrence).arr_unique;
-        return Array::from_shape_vec(IxDyn(&[arr_unique.len()]), arr_unique).unwrap();
-    }
+    let axis = match axis_flatten {
+        AxisFlatten::Dim(None) => {
+            let arr_flat = arr.flatten().to_vec();
+            let arr_unique = uniquetol(&arr_flat, tols, equal_nan, occurrence).arr_unique;
+            return Array::from_shape_vec(IxDyn(&[arr_unique.len()]), arr_unique).unwrap();
+        }
+        AxisFlatten::Dim(Some(axis)) => axis,
+    };
     
-    let dim = match axis {
-        Axis::Dim(value) => Some(value),
-        _ => panic!("Invalid axis"),
-    }.unwrap();
-    
-    let arrs_flat = arr.axis_iter(ndarray::Axis(dim));
+    let arrs_flat = arr.axis_iter(Axis(axis));
     // find largest subset s.t. no pair of (array) elements is element-wise approx. equal ...
     arr.clone() // placeholder return just so rust-analyzer doesn't yell at me :)
 }
