f8224d6360c14d354ef603d9e02af1780cc60e07
Luis-Varona <lbvarona@mta.ca>
2025-04-06 00:35:45 -0300

    Fixed up module names, and ready for a v0.1.0-beta release!
    Still need to add (lots more) docs and tests, but will relegate that to v0.1.0-rc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 mod isapprox;
 mod uniquetol_1d;
 mod uniquetol_nd;
-mod uniquetol;
+mod uniquetol_traits;
 
 pub use isapprox::{Tols, EqualNan};
 pub use uniquetol_1d::{Occurrence, UniqueTolArray};
 pub use uniquetol_nd::FlattenAxis;
-pub use uniquetol::{UniqueTol1D, UniqueTolND};
+pub use uniquetol_traits::{UniqueTol1D, UniqueTolND};
